79a1608c178ebaf095219cdef4c61cc62d8a2dd4
Emmatshi <etshi8712@gmail.com>
2026-01-02 15:23:31 -0600

    Rename app references to my-bc-st004

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="my-bc-st002",
+    page_title="my-bc-st004",
     layout="wide"
 )
 
-st.title("my-bc-st002")
+st.title("my-bc-st004")
 st.success("Streamlit + pyenv + Poetry is working 🎉")
